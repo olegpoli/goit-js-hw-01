b56018210379132307b83b9aebb63ba2f03d60c9
HP <zontiksm@ukr.net>
2024-07-14 14:40:27 +0300

    add index file

--- js/task-3.js
+++ js/task-3.js
@@ -12,3 +12,4 @@ function getElementWidth (content, padding, border) {
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
 console.log(getElementWidth("200px", "0px", "0px")); // 200
+
